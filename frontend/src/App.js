ecfe75bf3c6ad3d7d711faacfd13d2706bc36f14
munavath naveen <b192535@rgukt.ac.in>
2024-06-20 22:44:18 +0530

    initialcommit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello bacho
+     <p> it is bachos website </p>
+    </div>
+  );
+}
+
+export default App;
